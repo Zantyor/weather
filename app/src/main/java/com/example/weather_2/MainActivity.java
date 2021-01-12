56ed3c4e086433d339e3df26bbcce610462b4073
Julian Honma (MSI) <julian@appsmiles.fr>
2021-01-12 11:40:57 +0100

    changed welcome text with first name

--- app/src/main/java/com/example/weather_2/MainActivity.java
+++ app/src/main/java/com/example/weather_2/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.weather_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String firstName = "Julian";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mTextViewHomeGreeting = (TextView)findViewById(R.id.TextViewHomeGreeting);
+        String formattedText = String.format(getString(R.string.home_greeting), firstName);
+        mTextViewHomeGreeting.setText(formattedText);
+
     }
+
 }
